1baf016e90f64e922d4da2694750bc6d7c05b191
premanuj <mesubedianuj.gmail.com>
2018-08-17 13:14:31 +0545

    init repo

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 base_dir = os.path.abspath(os.path.dirname(__name__))
-postgres_local_base = 'postgresql://your_username:your_username@localhost/'
+postgres_local_base = 'postgresql://postgres:postgres@localhost/'
 database_name = 'flaskapi'
 
 
