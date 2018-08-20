aebd0c2f5dc9b70de1c4e138cf556a456ce11630
premanuj <mesubedianuj.gmail.com>
2018-08-20 13:47:21 +0545

    Auth Module

--- app/auth/models.py
+++ app/auth/models.py
@@ -2,7 +2,7 @@ from app import db
 
 
 class User(db.Model):
-    __tablename__ = 'providers'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     name = db.Column(db.String(50), nullable= False)
